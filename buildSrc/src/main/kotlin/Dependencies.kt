f7d05083713aaf8c47477e106703bea910e67315
zedd11 <dangocphuong11t3@gmail.com>
2022-10-03 11:04:17 +0700

    Add code

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,22 @@
+object BuildPlugins {
+    const val androidApplication = "com.android.application"
+    const val kotlinAndroid = "org.jetbrains.kotlin.android"
+    const val navigationSafeArgs = "androidx.navigation.safeargs.kotlin"
+    const val kapt = "kapt"
+}
+
+object AndroidSdk {
+    const val minSdk = 23
+    const val targetSdk = 32
+    const val compileSdk = 32
+}
+
+object AppConfig {
+    const val applicationId = "example.android.exam.examplelocal1"
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
+
+object Libraries {}
+object TestLibraries {}
